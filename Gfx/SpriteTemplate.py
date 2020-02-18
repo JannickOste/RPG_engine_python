dd84f6e7d1250b27fe3165db0835bb258d833a12
boedaka <oste.jannick@hotmail.com>
2020-02-18 12:24:45 +0100

    Add files via upload

--- Gfx/SpriteTemplate.py
+++ Gfx/SpriteTemplate.py
@@ -6,7 +6,6 @@ from Misc.Settings import *
 # Default wrapper for sprite template.
 class SpriteTemplate(pg.sprite.Sprite):
     def __init__(self, game, x, y, groups, image):
-
         self.groups = groups
         pg.sprite.Sprite.__init__(self, groups)
 
@@ -18,3 +17,15 @@ class SpriteTemplate(pg.sprite.Sprite):
 
         self.rect.x = x * TILESIZE
         self.rect.y = y * TILESIZE
+
+    def get_pos(self, raw=False):
+        if raw:
+            return self.rect.x, self.rect.y
+        else:
+            return self.rect.x//TILESIZE, self.rect.y//TILESIZE
+
+    def set_pos(self, x=None, y=None):
+        if x is not None:
+            self.rect.x = x
+        if y is not None:
+            self.rect.y = y
